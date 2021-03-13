182935c09c3730751b2f33f7935244139da045e0
Chengkai Yang <yangchengkai@gmail.com>
2021-03-13 17:15:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace EventHubTest
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         var hubEndPoint = "Endpoint=sb://chyaeventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=//JboujWToYmJ2tytgSKApsCgERt2OC4O4/H5uXD7CQ=";
+         Console.WriteLine("Event Hub event publisher...");
+
+
+         var hub = EventHubClient.Create(new EventHubsConnectionStringBuilder(hubEndPoint)
+                                         {
+                                            EntityPath = "chyahub"
+                                         });
+
+         Console.WriteLine("enter event data (Q to quit):");
+         while (true)
+         {
+            var data = Console.ReadLine();
+
+            if (data == "Q")
+            {
+               break;
+            }
+
+            await hub.SendAsync(new EventData(Encoding.UTF8.GetBytes($"Event data: {data} at {DateTime.Now}")));
+         }
+
+      }
+   }
+}
